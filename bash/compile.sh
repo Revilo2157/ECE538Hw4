53c39519eadd24643024d5a651f9f81787aff68b
Oliver Rodas <oar5@vlsi05.egr.duke.edu>
2021-04-27 02:21:10 -0400

    Added

--- bash/compile.sh
+++ bash/compile.sh
@@ -1,7 +1,22 @@
+if [ $# -ne 2 ]; then
+	echo "Need top level module name and source directory"
+	exit 1
+fi
+
 export top=$1
 export source=$(realpath $2)
 
 cd compilation 
-dc_shell -f DC_script.tcl
-dc_shell -f scan_insertion_DC.tcl
-cp ${top}_scan.v gate_level
+
+# Compile the Verilog
+echo "Compiling"
+dc_shell -f DC_script.tcl > compile.txt
+grep Error compile.txt
+
+# Insert Scan
+echo "Inserting Scan Cells"
+dc_shell -f scan_insertion_DC.tcl > scan.txt
+grep Error scan.txt
+
+mkdir ../gate/$top > /dev/null
+cp ${top}_scan.v ../gate/$top
